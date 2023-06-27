b0dc60a8135aaefb785e1ada5a1f60ded3b351ad
daidoan774 <75259385+daidoan774@users.noreply.github.com>
2023-06-27 18:26:33 +0700

    Update for Ass2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,182 +3,103 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.openqa.selenium.support.ui.Select;
+import java.time.Duration;
+import java.util.List;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static ChromeDriver driver = new ChromeDriver();
+    public static WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
     public static void main(String[] args) throws InterruptedException {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        ChromeDriver driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        Thread.sleep(10000);
-        WebElement name = driver.findElement(By.xpath("//input[@name='username']"));
-        System.out.println(name.getText());
-        name.sendKeys("Admin");
+        getTex("//input[@name='username']", "Admin");
+        getTex("//input[@name='password']", "admin123");
+        click("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']");
 
-        WebElement pass = driver.findElement(By.xpath("//input[@name='password']"));
-        System.out.println(name.getText());
-        pass.sendKeys("admin123");
+        click("//a[@class='oxd-main-menu-item']/span[text()='My Info']");
+//        clea("//label[text()='Employee Full Name']/../following-sibling::div//input[@name='firstName']");
+//        clea("//label[text()='Employee Full Name']/../following-sibling::div//input[@name='middleName']");
+//        clea("//label[text()='Employee Full Name']/../following-sibling::div//input[@name='lastName']");
 
-        WebElement button = driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']"));
-        System.out.println(button.getText());
-        button.click();
+        getTex("//label[text()='Employee Full Name']/../following-sibling::div//input[@name='firstName']", "Automation");
+        getTex("//label[text()='Employee Full Name']/../following-sibling::div//input[@name='middleName']", "22");
+        getTex("//label[text()='Employee Full Name']/../following-sibling::div//input[@name='lastName']", "5");
 
-//        Thread.sleep(5000);
-//        WebElement proTime = driver.findElement(By.xpath("//div[@class='orangehrm-attendance-card-profile']"));
-//        System.out.println(proTime.getText());
-//        proTime.getText();
-//
-//        WebElement admin = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Admin']"));
-//        System.out.println(admin.getText());
-//        admin.click();
-//
-//        Thread.sleep(5000);
-//        WebElement pim = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='PIM']"));
-//        System.out.println(pim.getText());
-//        pim.click();
-//
-//        Thread.sleep(5000);
-//        WebElement leave = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Leave']"));
-//        System.out.println(leave.getText());
-//        leave.click();
-//
-//        Thread.sleep(5000);
-//        WebElement time = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Time']"));
-//        System.out.println(time.getText());
-//        time.click();
-//
-//        Thread.sleep(5000);
-//        WebElement recruit = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Recruitment']"));
-//        System.out.println(recruit.getText());
-//        recruit.click();
-//
-//        Thread.sleep(5000);
-//        WebElement info = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='My Info']"));
-//        System.out.println(info.getText());
-//        info.click();
-//
-//        Thread.sleep(5000);
-//        WebElement perform = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Performance']"));
-//        System.out.println(perform.getText());
-//        perform.click();
-//
-//        Thread.sleep(5000);
-//        WebElement directory = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Directory']"));
-//        System.out.println(directory.getText());
-//        directory.click();
-//
-//        Thread.sleep(5000);
-//        WebElement maintenance = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Maintenance']"));
-//        System.out.println(maintenance.getText());
-//        maintenance.click();
-//
-////        driver.navigate().back();
-//
-//        Thread.sleep(5000);
-//        WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
-//        System.out.println(password.getText());
-//        password.sendKeys("admin123");
-//
-//        WebElement btn = driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--large oxd-button--secondary orangehrm-admin-access-button']"));
-//        System.out.println(btn.getText());
-//        btn.click();
-
-        Thread.sleep(5000);
-        WebElement buzz = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Buzz']"));
-        System.out.println(buzz.getText());
-        buzz.click();
-
-        Thread.sleep(5000);
-        WebElement dashboard = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Dashboard']"));
-        System.out.println(dashboard.getText());
-        dashboard.click();
-
-//        Thread.sleep(5000);
-//        WebElement request = driver.findElement(By.xpath("//p[text()=' (11) Leave Requests to Approve']"));
-//        System.out.println(request.getText());
-//        request.click();
-//
-//        Thread.sleep(5000);
-//        driver.navigate().back();
-//
-//        Thread.sleep(5000);
-//        WebElement timesheet = driver.findElement(By.xpath("//p[text()=' (9) Timesheets to Approve']"));
-//        System.out.println(timesheet.getText());
-//        timesheet.click();
-//
-//        Thread.sleep(5000);
-//        driver.navigate().back();
-
-        Thread.sleep(5000);
-        WebElement pending = driver.findElement(By.xpath("//p[text()='(1) Pending Self Review']"));
-        System.out.println(pending.getText());
-        pending.click();
-
-        Thread.sleep(5000);
-        driver.navigate().back();
-
-//        Thread.sleep(5000);
-//        WebElement candidate = driver.findElement(By.xpath("//p[text()='(1) Candidate to Interview']"));
-//        System.out.println(candidate.getText());
-//        candidate.click();
-//
-//        Thread.sleep(5000);
-//        driver.navigate().back();
+        getTex("//label[text()='Nickname']/../following-sibling::div//input[@class='oxd-input oxd-input--active']", "Testing");
+        getTex("//label[text()='Employee Id']/../following-sibling::div//input[@class='oxd-input oxd-input--active']","22");
+        getTex("//label[text()='Other Id']/../following-sibling::div//input[@class='oxd-input oxd-input--active']","220");
+        getTex("//label[text()=concat('Driver', \"'\", 's License Number')]/../following-sibling::div//input[@class='oxd-input oxd-input--active']", "11111");
+        getdaTe("//label[text()='License Expiry Date']/../following-sibling::div//input[@placeholder='yyyy-mm-dd']");
+        getTex("//label[text()='SSN Number']/../following-sibling::div//input[@class='oxd-input oxd-input--active']","300");
+        getSelect("//label[text()='Nationality']/../following::div//div[@class='oxd-select-text oxd-select-text--active']","Afghan");
+        getSelect("//label[text()='Marital Status']/../following::div//div[@class='oxd-select-wrapper']//div[@class='oxd-select-text oxd-select-text--active']//div[@clear='false']","Single");
+        getdaTe("//label[text()='Date of Birth']/../following-sibling::div//input[@class='oxd-input oxd-input--active']");
+        getRadio("//label[text()='Gender']/../following-sibling::div//div[@class='oxd-radio-wrapper']//input[@type='radio' and @value ='1']");
+        getTex("//label[text()='Military Service']/../following-sibling::div//input[@class='oxd-input oxd-input--active']","1");
+        click("//div[@class='oxd-checkbox-wrapper']//input[@type='checkbox']");
+//        click("//div[@class='oxd-topbar-header-userarea']/child::ul/child::li//child::span[@class='oxd-userdropdown-tab']");
+    }
 
 
-        Thread.sleep(10000);
-        WebElement assignleave = driver.findElement(By.xpath("//p[text()='Assign Leave']"));
-        System.out.println(assignleave.getText());
-        assignleave.click();
+    public static void click(String xpath) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        WebElement element = driver.findElement(By.xpath(xpath));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
 
-        Thread.sleep(10000);
-        driver.navigate().back();
-//        Thread.sleep(5000);
-//        WebElement dashboard = driver.findElement(By.xpath("//a[@class='oxd-main-menu-item']/span[text()='Dashboard']"));
-//        System.out.println(dashboard.getText());
-//        dashboard.click();
+    public static void getTex(String xpath, String text) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        WebElement element = driver.findElement(By.xpath(xpath));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
 
-        Thread.sleep(10000);
-        WebElement leavelist = driver.findElement(By.xpath("//p[text()='Leave List']"));
-        System.out.println(leavelist.getText());
-        leavelist.click();
+        while (element.getAttribute("value").isEmpty()) {
+            element.sendKeys(text);
+        }
+        element.clear();
+    }
+    public static void getdaTe(String xpath)
+    {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        WebElement element = driver.findElement(By.xpath(xpath));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
 
-        Thread.sleep(5000);
-        driver.navigate().back();
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = localDate.format(time);
+        element.clear();
+        element.sendKeys(formattedDate);
+    }
 
-        Thread.sleep(5000);
-        WebElement timesheets = driver.findElement(By.xpath("//p[text()='Timesheets']"));
-        System.out.println(timesheets.getText());
-        timesheets.click();
+    public static void getSelect(String xpath,String num) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        WebElement element = driver.findElement(By.xpath(xpath));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
 
-        Thread.sleep(5000);
-        driver.navigate().back();
+        Select select = new Select(element);
+        select.selectByVisibleText(num);
+    }
+    public static void getRadio(String xpath) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        WebElement radioBtn = driver.findElement(By.xpath(xpath));
+        wait.until(ExpectedConditions.elementToBeClickable(radioBtn));
 
-        Thread.sleep(5000);
-        WebElement applyleave = driver.findElement(By.xpath("//p[text()='Apply Leave']"));
-        System.out.println(applyleave.getText());
-        applyleave.click();
+        String value = radioBtn.getAttribute("value");
+        boolean isSelected = radioBtn.isSelected();
 
-        Thread.sleep(5000);
-        driver.navigate().back();
+        if (!isSelected || !value.equals("1")) {
+            radioBtn.click();
+            }
 
-        Thread.sleep(5000);
-        WebElement myleave = driver.findElement(By.xpath("//p[text()='My Leave']"));
-        System.out.println(myleave.getText());
-        myleave.click();
-
-        Thread.sleep(5000);
-        driver.navigate().back();
-
-        Thread.sleep(5000);
-        WebElement mytimesheet = driver.findElement(By.xpath("//p[text()='My Timesheets']"));
-        System.out.println(mytimesheet.getText());
-        mytimesheet.click();
-
-        Thread.sleep(5000);
-        driver.navigate().back();
+       }
 
     }
-}
+
+
+
+
